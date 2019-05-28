38b122da8fc448ae2d22041ca6f248c2ebd52cca
Sebastien <seb75@fedoraproject.org>
2019-05-28 11:29:57 +0200

    Finished router feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './App.css';
 import ItemList from './components/ItemList'
+import About from './components/About'
 import NavigationBar from './components/NavigationBar'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
+    const routes = [
+        {
+            path: "/",
+            component: ItemList
+        },
+        {
+            path: "/about",
+            component: About
+        }
+    ];
+
     return (
         <div className="App">
             <NavigationBar/>
-            <ItemList />
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={ItemList} />
+                    <Route path='/about' render={props => <About {...props} />}/>
+                </Switch>
+            </Router>
         </div>
     );
 }

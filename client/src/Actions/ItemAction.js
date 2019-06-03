274bacb6ebcb054b117f5c7b63cb53c8de281c36
Sebastien <seb75@fedoraproject.org>
2019-06-03 12:02:53 +0200

    created redux store

---
+++ client/src/Actions/ItemAction.js
@@ -0,0 +1,20 @@
+/*
+    Action Type
+ */
+
+const ADD_ITEM = 'ADD_ITEM';
+const REMOVE_ITEM = 'REMOVE_ITEM';
+
+/*
+    Function
+ */
+export function getItem(items) {
+    return {type: 'GET_ITEM', value: items}
+}
+export function addItemToStore(item) {
+    return {type: ADD_ITEM, value: item}
+}
+
+export function removeItem(itemID) {
+    return {type: REMOVE_ITEM, value: itemID}
+}
